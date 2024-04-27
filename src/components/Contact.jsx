9f6df8f6dc1e79b6db07bf80bd573209bc41aea0
Alimisolution <alimirosheed5000@gmail.com>
2024-04-27 03:52:19 +0100

    Added mobile mneu

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,21 @@
+
+import {FaGithub, FaTwitter, FaLinkedin} from 'react-icons/fa'
 function Contact(){
     return(
         <div className="px-4 md:py-24 py-20 text-center bg-slate-800 text-white md:mt-0" id="contact">
             <div className="max-w-3xl mx-auto">
             <h1 className="md:text-5xl text-4xl mb-10 font-semibold">Contact Me</h1>
-            <p className="font-semibold text-lg">Contact: <span className="font-normal">08080755931</span></p>
-            <p className="font-semibold text-lg">Mail: <span className="font-normal">alimirosheed5000@gmail.com</span></p>
+           <div className="flex justify-center items-center gap-10">
+            <a href="https://github.com/Alimisolution?tab=repositories" target='_blank' className="font-bold md:text-4xl text-2xl">
+                <FaGithub/>
+            </a>
+             <a href="https://twitter.com/alimi_rosh55817" target='_blank' className="font-bold md:text-4xl text-2xl">
+                <FaTwitter/>
+            </a>
+             <a href="https://www.linkedin.com/in/alimi-rosheed-271a5423a/" target='_blank' className="font-bold md:text-4xl text-2xl">
+                <FaLinkedin/>
+            </a>
+           </div>
             </div>
             <a href="#home" className="fixed bottom-28 right-5 z-10 bg-slate-800 text-white w-12 h-12 text-xl flex justify-center items-center border-2 font-bold rounded-full">&uarr;</a>
         </div>
--- src/components/MobileMneu.jsx
+++ src/components/MobileMneu.jsx
@@ -1,11 +1,27 @@
+import {FaHome} from 'react-icons/fa';
+import { SiAboutdotme } from "react-icons/si";
+import { GiSkills } from "react-icons/gi";
+import { GrProjects } from "react-icons/gr";
+import { IoIosContact } from "react-icons/io";
+
 function MobileMneu() {
      return (
         <nav className="bg-slate-900 py-6 px-4 flex items-center justify-between md:hidden text-sm font-bold text-white fixed bottom-0 z-20 left-0 right-0">
-                  <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#skills">Skills</a>
-                <a href="#projects">Projects</a>
-                <a href="#contact">Contact</a>
+                  <a href="#home" className='font-bold text-2xl' >
+                    <FaHome/>
+                  </a>
+                <a href="#about" className='font-bold text-2xl' >
+                <SiAboutdotme/>
+                </a>
+                <a href="#skills" className='font-bold text-2xl' >
+                  <GiSkills/>
+                </a>
+                <a href="#projects" className='font-bold text-2xl' >
+                  <GrProjects/>
+                </a>
+                <a href="#contact" className='font-bold text-2xl' >
+                  <IoIosContact/>
+                </a>
 
             
         </nav>
