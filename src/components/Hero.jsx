8a7a524a1e6a26112d89615dc378751e52b5819d
Alimisolution <alimirosheed5000@gmail.com>
2024-04-29 17:27:41 +0100

    change the mobil menu

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,10 @@ function Hero() {
             <h1 className="font-bold text-lg md:text-2xl mb-10 mt-5">{text3}</h1>
               <div className="space-y-1 flex items-center gap-5 justify-center md:justify-start">
               <a href="/img/alimisolution.jpg" download className="border-2 border-blue-300 py-3 md:px-12 px-5 rounded-2xl text-sm ">Download My Resume</a>
-              <a href="mailto:alimirosheed5000@gmail.com" className="border-2 border-blue-300 py-3 md:px-12 px-5 rounded-2xl text-sm ">Mail Me</a>
+               <div className="space-y-1 border-2 border-blue-300 py-2 sm:px-8 px-4 rounded-2xl">
+              <a href="whatsapp://send?phone=+2348080755931">Hire Me</a>
+            </div>
+              
              
             </div>
         </div>
--- src/components/MobileMneu.jsx
+++ src/components/MobileMneu.jsx
@@ -6,21 +6,41 @@ import { IoIosContact } from "react-icons/io";
 
 function MobileMneu() {
      return (
-        <nav className="bg-slate-900 py-6 px-4 flex items-center justify-between md:hidden text-sm font-bold text-white fixed bottom-0 z-20 left-0 right-0">
-                  <a href="#home" className='font-bold text-2xl' >
+        <nav className="bg-slate-900 py-6 px-2 flex items-center justify-between md:hidden text-sm font-bold text-white fixed bottom-0 z-20 left-0 right-0">
+                  <a href="#home" className='font-bold text-2xl flex flex-col items-center gap-1' >
                     <FaHome/>
+                    <small className='text-sm'>
+
+                    Home
+                    </small>
                   </a>
-                <a href="#about" className='font-bold text-2xl' >
+                <a href="#about" className='font-bold text-2xl flex flex-col items-center gap-1' >
                 <SiAboutdotme/>
+                <small className='text-sm'>
+
+                About
+                </small>
                 </a>
-                <a href="#skills" className='font-bold text-2xl' >
+                <a href="#skills" className='font-bold text-2xl flex flex-col items-center gap-1' >
                   <GiSkills/>
+                  <small className='text-sm'>
+
+                  Skills
+                  </small>
                 </a>
-                <a href="#projects" className='font-bold text-2xl' >
+                <a href="#projects" className='font-bold text-2xl flex flex-col items-center gap-1' >
                   <GrProjects/>
+                  <small className='text-sm'>
+
+                  Projects
+                  </small>
                 </a>
-                <a href="#contact" className='font-bold text-2xl' >
+                <a href="#contact" className='font-bold text-2xl flex flex-col items-center gap-1' >
                   <IoIosContact/>
+                  <small className='text-sm'>
+
+                  Contact
+                  </small>
                 </a>
 
             
