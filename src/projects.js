fad8df9b3f71ed94f8e44b96995ebe58914b6f1e
Alimisolution <alimirosheed5000@gmail.com>
2024-08-20 10:44:30 +0100

    Added-New-Project

--- src/projects.js
+++ src/projects.js
@@ -1,11 +1,12 @@
 const  Projects = [
-    {id: 1, image: "/img/proshop.png", name: "proShop", code : "Mern Stack", link: "https://proshop-hwl2.onrender.com/", status: 'Live'},
-    {id: 2, image: "/img/jobify.png", name: "Jobify", code : "Mern Stack", link: "https://jobify-qiz6.onrender.com/", status: 'Live'},
-    {id: 3, image: "/img/hoobank.png", name: "Hoobank", code: "React-vite + Tailwind", link: "https://hoobank-site-react.netlify.app/", status: 'Live'},
-    {id: 4, image: "/img/gitfinder.png", name: "Github Finder", code: "Html + Css + Js", link: "https://github-user-search-using-input.netlify.app/", status: 'Live'},
-    {id: 5, image: "/img/bootcamp.png", name: "Bootcamp", code: "Bootstrap", link: "https://bootcamp-bootstra.netlify.app/", status: 'Live'},
-    {id: 6, image: "/img/teasite.png", name: "Tea-Site", code: "Html + Css + Js", link: "https://alimiteasite.netlify.app/", status: 'Live'},
-    {id: 7, image: "/img/rshoe.png",name: "R-Shoe", code: "Html + Css + Js", link: "https://rshoe.netlify.app/", status: 'Live'},
+    {id: 1, image: "/img/e-store.png", name: "E-Store", code : "React-Vite  Tailwind + Api", link: "https://e-store-web.netlify.app/", status: 'Live'},
+    {id: 2, image: "/img/proshop.png", name: "proShop", code : "Mern Stack", link: "https://proshop-hwl2.onrender.com/", status: 'Live'},
+    {id: 3, image: "/img/jobify.png", name: "Jobify", code : "Mern Stack", link: "https://jobify-qiz6.onrender.com/", status: 'Live'},
+    {id: 4, image: "/img/hoobank.png", name: "Hoobank", code: "React-vite + Tailwind", link: "https://hoobank-site-react.netlify.app/", status: 'Live'},
+    {id: 5, image: "/img/gitfinder.png", name: "Github Finder", code: "Html + Css + Js", link: "https://github-user-search-using-input.netlify.app/", status: 'Live'},
+    {id: 6, image: "/img/bootcamp.png", name: "Bootcamp", code: "Bootstrap", link: "https://bootcamp-bootstra.netlify.app/", status: 'Live'},
+    {id: 7, image: "/img/teasite.png", name: "Tea-Site", code: "Html + Css + Js", link: "https://alimiteasite.netlify.app/", status: 'Live'},
+    {id: 8, image: "/img/rshoe.png",name: "R-Shoe", code: "Html + Css + Js", link: "https://rshoe.netlify.app/", status: 'Live'},
 ]
 
 export default Projects
